ae7aa8976d5d361d164f7d5f774448037dc99601
paramireze <paramireze@gmail.com>
2016-08-03 17:24:52 -0500

    bootstrapped jobs

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -31,11 +31,14 @@ class BootStrap {
 
         baseline.addAll(informationTechnology, humanResources, healthCare, finance, adminstrative)
 
-        baseline << new EmploymentType('Full Time')
-        baseline << new EmploymentType('Part Time')
-        baseline << new EmploymentType('LTE')
-        baseline << new EmploymentType('Project Hire')
-        baseline << new EmploymentType('Student')
+        EmploymentType fullTime = new EmploymentType('Full Time')
+        EmploymentType partTime = new EmploymentType('Part Time')
+        EmploymentType lte = new EmploymentType('LTE')
+        EmploymentType project = new EmploymentType('Project')
+        EmploymentType student = new EmploymentType('Student')
+        EmploymentType internship = new EmploymentType('Internship')
+
+        baseline.addAll(fullTime, partTime, lte, project, student, internship)
 
         User paul = new User('paul', 'password').save()
         User bryan =  new User('bryan', 'password')
@@ -60,12 +63,61 @@ class BootStrap {
         baseline << new DocumentType('Thank You Letter')
         baseline << new DocumentType('Follow Up')
 
-        baseline << new Job('Software Developer', informationTechnology)
-        baseline << new Job('Help Desk', informationTechnology)
-        baseline << new Job('Finance Specialist', finance)
-        baseline << new Job('Human Resource Director', humanResources)
-        baseline << new Job('Nurse', healthCare)
-        baseline << new Job('Resident Assistant', healthCare)
+        Job softwareDeveloper = new Job('Software Developer', informationTechnology)
+        Job helpDesk = new Job('Help Desk', informationTechnology)
+        Job financialSpec = new Job('Finance Specialist', finance)
+        Job humanResourceDirector = new Job('Human Resource Director', humanResources)
+        Job nurse = new Job('Nurse', healthCare)
+        Job residentAssistant = new Job('Resident Assistant', healthCare)
+
+        baseline.addAll(softwareDeveloper, helpDesk, financialSpec, humanResourceDirector, nurse, residentAssistant)
+
+        //job, employmentType, start, end, salaryRange, active (bool)
+        JobPost softwareDev = new JobPost(
+                job: softwareDeveloper,
+                employmentType: fullTime,
+                postStart: 'July 01, 2016',
+                postEnd: 'Sept 01, 2016',
+                salaryRange: '50,000 - 80,000',
+                active: true
+        )
+
+        JobPost support = new JobPost(
+                job:helpDesk,
+                employmentType: partTime,
+                postEnd: 'Oct 01, 2016',
+                postStart: 'June 01, 2016',
+                salaryRange: '40,000 - 60,000',
+                active: true
+        )
+
+        JobPost hrStaff = new JobPost(
+                job:humanResourceDirector,
+                employmentType: fullTime,
+                postEnd: 'Oct 01, 2016',
+                postStart: 'June 01, 2016',
+                salaryRange: '43,000 - 65,000',
+                active: true
+        )
+
+        JobPost nurseProfessional = new JobPost(
+                job:nurse,
+                employmentType: fullTime,
+                postEnd: 'Oct 01, 2016',
+                postStart: 'June 01, 2016',
+                salaryRange: '44,000 - 95,000',
+                active: true
+        )
+        JobPost residentAss = new JobPost(
+                job:residentAssistant,
+                employmentType: partTime,
+                postEnd: 'Oct 11, 2016',
+                postStart: 'June 21, 2016',
+                salaryRange: '34,000 - 45,000',
+                active: true
+        )
+
+        baseline.addAll(softwareDev, support, softwareDev,hrStaff, nurseProfessional, residentAss)
 
         baseline.each {
             println "saving: $it"
