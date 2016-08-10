70bd9d40f5f8050740e28da18927756fdf4c0209
paramireze <paramireze@gmail.com>
2016-08-09 21:28:46 -0500

    added additional properties to document domain

--- grails-app/domain/jobs/Document.groovy
+++ grails-app/domain/jobs/Document.groovy
@@ -2,16 +2,29 @@ package jobs
 
 class Document {
     DocumentType type
+    User user
+    String title
+    String body
     String filePath
 
 
 
     static constraints = {
+        body nullable:true
         filePath nullable:true
     }
+
+    Document(DocumentType type,User user, String title, String body) {
+        this()
+        this.type = type
+        this.user = user
+        this.title = title
+        this.body = body
+    }
+
 
     static belongsTo = [user: User]
     static hasOne = DocumentType
 
-    String toString() {"$type"}
+    String toString() {"$title"}
 }
