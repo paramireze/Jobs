4bf0ac6eb224e2f136bd94eedb44ab90604fce16
paramireze <paramireze@gmail.com>
2016-08-23 02:21:20 -0500

    added property for adding text manually

--- grails-app/domain/jobs/Application.groovy
+++ grails-app/domain/jobs/Application.groovy
@@ -7,21 +7,24 @@ class Application {
     Status status
     Document resume
     Document coverLetter
+    String resumeText
 
     static constraints = {
         jobPost nullable: false
         status nullable: false
         resume nullable: true
         coverLetter nullable: true
+        resumeText nullable: true
     }
 
-    Application(User user, JobPost jobPost, Document resume, Document coverLetter, Status status) {
+    Application(User user, JobPost jobPost, Document resume, Document coverLetter, Status status, resumeText) {
         this()
         this.user = user
         this.jobPost = jobPost
         this.resume = resume
         this.coverLetter = coverLetter
         this.status = status
+        this.resumeText = resumeText
     }
 
     static belongsTo = [user:User]
